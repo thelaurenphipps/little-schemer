b89959df886c248dd9b3d252daa57a9279e29608
req91236 <lauren.phipps@mutualofomaha.com>
2021-05-07 13:18:39 -0500

    updating chapter 8 output based on josh's thought experiment

--- Chapter8.rkt
+++ Chapter8.rkt
@@ -747,6 +747,33 @@
 
 (define the-last-friend
   (lambda (newl product sum)
-    (cons sum (cons product (cons product newl)))))
+    (cons sum (cons product newl))))
 
-(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
+
+                '(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
+
+'(evens-only*&co '(9 1 2 8) the-last-friend)
+(evens-only*&co '(9 1 2 8) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8)) the-last-friend)
+(evens-only*&co '((9 1 2 8)) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3 10) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3 10 ((9 9))) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9))) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7)) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7)) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6)) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6)) the-last-friend)
+
+'(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
+(evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
